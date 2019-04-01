de8fffb67965d6e5f40c23ec592678b856379f20
Jannis R <mail@jannisr.de>
2019-04-01 15:52:50 +0200

    implementation :zap:

--- index.js
+++ index.js
@@ -1,3 +1,123 @@
 'use strict'
 
-// todo
+const squareGrid = require('@turf/square-grid').default
+const distance = require('@turf/distance').default
+const createAvgWindow = require('live-moving-average')
+const PQueue = require('p-queue')
+const throttle = require('lodash.throttle')
+const {EventEmitter} = require('events')
+
+const MINUTE = 60 * 1000
+const MAX_TILE_SIZE = 5000 // 5km
+
+const roundTo = (x, d) => parseFloat(x.toFixed(d))
+const computeTiles = (bbox, cellSize) => {
+	const grid = squareGrid([bbox.west, bbox.south, bbox.east, bbox.north], 1000, {units: 'meters'})
+	return grid.features.map((f) => {
+		const coords = f.geometry.coordinates[0]
+		return {
+			north: roundTo(coords[2][1], 4),
+			west: roundTo(coords[0][0], 4),
+			south: roundTo(coords[0][1], 4),
+			east: roundTo(coords[2][0], 4)
+		}
+	})
+}
+
+const createMonitor = (hafas, bbox, interval, concurrency = 8) => {
+	if (!hafas || 'function' !== typeof hafas.radar || 'function' !== typeof hafas.trip) {
+		throw new Error('Invalid HAFAS client passed.')
+	}
+
+	if ('number' !== typeof bbox.north) throw new TypeError('bbox.north must be a number.')
+	if ('number' !== typeof bbox.west) throw new TypeError('bbox.west must be a number.')
+	if ('number' !== typeof bbox.south) throw new TypeError('bbox.south must be a number.')
+	if ('number' !== typeof bbox.east) throw new TypeError('bbox.east must be a number.')
+	if (bbox.north <= bbox.south) throw new Error('bbox.north must be larger than bbox.south.')
+	if (bbox.east <= bbox.west) throw new Error('bbox.east must be larger than bbox.west.')
+	const tiles = computeTiles(bbox)
+
+	const discoverInterval = Math.max(interval, Math.min(5 * interval, 3 * MINUTE))
+	const trackInterval = interval
+
+	const queue = new PQueue({concurrency: 8})
+	const trips = new Map()
+
+	let nrOfTrips = 0, reqs = 0
+	const avgReqDuration = createAvgWindow(10, 300)
+	const reportStats = throttle(() => { // todo: throttle
+		out.emit('stats', {reqs, avgReqDuration: avgReqDuration.get(), trips: nrOfTrips})
+	}, 1000)
+	const onReqTime = (reqTime) => {
+		reqs++
+		avgReqDuration.push(reqTime)
+		reportStats()
+	}
+
+	const fetchTile = (tile) => async () => {
+		let movements
+		try {
+			const t0 = Date.now()
+			movements = await hafas.radar(tile, {
+				results: 1000, frames: 0, polylines: false // todo: `opt.language`
+			})
+			onReqTime(Date.now() - t0)
+		} catch (err) {
+			out.emit('error', err)
+			return
+		}
+
+		for (const m of movements) {
+			out.emit('position', m.location, m)
+
+			if (trips.has(m.tripId)) continue
+			trips.set(m.tripId, m.line && m.line.name || 'foo')
+			nrOfTrips++
+			out.emit('new-trip', m.tripId, m)
+			// todo: processs `m.nextStopovers`
+		}
+	}
+
+	const fetchAllTiles = async () => {
+		try {
+			await queue.addAll(tiles.map(fetchTile), {priority: 1})
+		} catch (err) {
+			out.emit('error', err)
+		}
+		// todo: call `fetchAllTrips`?
+		setTimeout(fetchAllTiles, discoverInterval)
+	}
+	setImmediate(fetchAllTiles)
+
+	// todo: remove trip if not found or over
+	const fetchTrip = (tripId, lineName) => async () => {
+		let trip
+		try {
+			const t0 = Date.now()
+			trip = await hafas.trip(tripId, lineName) // todo: `opt.language`
+			onReqTime(Date.now() - t0)
+		} catch (err) {
+			out.emit('error', err)
+			return
+		}
+
+		out.emit('trip', trip)
+		for (const stopover of trip.stopovers) out.emit('stopover', stopover, trip)
+	}
+
+	const fetchAllTrips = () => {
+		// todo: check for "queue congestion"
+		for (const [tripId, lineName] of trips.entries()) {
+			queue.add(fetchTrip(tripId, lineName)) // todo: rejection?
+		}
+		setTimeout(fetchAllTrips, trackInterval)
+	}
+	setImmediate(fetchAllTrips)
+
+	// todo: queue on error?
+	const out = new EventEmitter()
+	// todo: stop function
+	return out
+}
+
+module.exports = createMonitor
