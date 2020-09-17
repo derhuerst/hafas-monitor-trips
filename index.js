9e2cb1627fc3e26f5a4ab383706c7795df770068
Jannis R <mail@jannisr.de>
2020-09-17 19:10:57 +0200

    remove queue, wait for all fetches 💥

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const debug = require('debug')('hafas-monitor-trips')
 const debugTrips = require('debug')('hafas-monitor-trips:trips')
 const debugFetch = require('debug')('hafas-monitor-trips:fetch')
-const {default: PQueue} = require('p-queue')
 const throttle = require('lodash.throttle')
 const {EventEmitter} = require('events')
 const computeTiles = require('./lib/compute-tiles')
@@ -14,94 +13,109 @@ const SECOND = 1000
 const MINUTE = 60 * SECOND
 const MAX_TILE_SIZE = 5 // in kilometers
 
-const WATCH_EVENTS = [
-	'trip', 'new-trip', 'trip-obsolete',
-	'stopover',
-	'position',
-	'stats'
-]
+const TOO_MANY_QUEUED_MSG = `\
+There are too many pending requests for the tile/trip fetching \
+intervals to be adhered to. Consider monitoring a smaller bbox or \
+increasing the request throughput.\
+`
 
-const LONG_QUEUE_MSG = 'many queued requests, consider monitoring a smaller' +
-	' bbox or increasing the concurrency'
-
-const createMonitor = (hafas, bbox, fetchTripsInterval = MINUTE, concurrency = 8, maxTileSize = 5) => {
+const createMonitor = (hafas, bbox, opt) => {
 	if (!hafas || 'function' !== typeof hafas.radar || 'function' !== typeof hafas.trip) {
 		throw new Error('Invalid HAFAS client passed.')
 	}
 
+	const {
+		fetchTripsInterval,
+		maxTileSize,
+	} = {
+		fetchTripsInterval: MINUTE,
+		maxTileSize: 5, // km
+		...opt,
+	}
+	const fetchTilesInterval = Math.max(
+		Math.min(fetchTripsInterval, 3 * MINUTE),
+		30 * SECOND,
+	)
+	debug('fetchTilesInterval', fetchTilesInterval)
+
 	const tiles = computeTiles(bbox, {maxTileSize})
 	debug('tiles', tiles)
 
-	const fetchTilesInterval = Math.max(Math.min(fetchTripsInterval, 3 * MINUTE), 30 * SECOND)
-	debug('fetchTilesInterval', fetchTilesInterval)
+	const out = new EventEmitter()
 
-	const queue = new PQueue({concurrency: 8})
 	const trips = new Map()
-
 	let nrOfTrips = 0
+	const tripSeen = (id, lineName) => {
+		if (trips.has(id)) return;
+		debugTrips('unknown trip, adding', id, lineName)
+		trips.set(id, lineName)
+		nrOfTrips++
+	}
+	const tripObsolete = (id) => {
+		if (!trips.has(id)) return;
+		debugTrips('obsolete trip, removing', id)
+		trips.delete(id)
+		nrOfTrips--
+	}
+
 	const reqCounter = createReqCounter()
 	const reportStats = throttle(() => {
+		const tSinceFetchAllTiles = Date.now() - tLastFetchTiles
+		const tSinceFetchAllTrips = Date.now() - tLastFetchTrips
 		out.emit('stats', {
 			...reqCounter.getStats(),
-			queuedReqs: queue.size,
-			trips: nrOfTrips,
-			tiles: tiles.length
+			running,
+			nrOfTrips,
+			nrOfTiles: tiles.length,
+			tSinceFetchAllTiles, tSinceFetchAllTrips,
 		})
-		if (queue.size > trips * 1.5) debug(LONG_QUEUE_MSG)
+		if (
+			tSinceFetchAllTiles > fetchTilesInterval * 1.5 ||
+			tSinceFetchAllTrips > fetchTripsInterval * 1.5
+		) {
+			out.emit('too-many-queued')
+			debug(TOO_MANY_QUEUED_MSG)
+		}
 	}, 1000)
 	const onReqTime = (reqTime) => {
 		reqCounter.onReqTime(reqTime)
-		reportStats()
+		reportStats().catch(err => out.emit('error', err))
 	}
 
-	const fetchTile = (tile) => async () => {
+	const fetchTile = async (tile) => {
 		debugFetch('fetching tile', tile)
 
 		const t0 = Date.now()
-		let movements
-		try {
-			movements = await hafas.radar(tile, {
-				results: 1000, duration: 0, frames: 0, polylines: false // todo: `opt.language`
-			})
-		} catch (err) {
-			out.emit('error', err)
-			return
-		}
+		const movements = await hafas.radar(tile, {
+			results: 1000, duration: 0, frames: 0, polylines: false,
+			// todo: `opt.language`
+		})
 		onReqTime(Date.now() - t0)
 
 		for (const m of movements) {
-			out.emit('position', m.location, m)
+			const lineName = m.line && m.line.name || 'foo'
+			const loc = m.location
+			debugFetch(m.tripId, lineName, loc.latitude, loc.longitude)
 
-			if (trips.has(m.tripId)) continue
-			debugTrips('unknown trip, adding', m.tripId)
-			trips.set(m.tripId, m.line && m.line.name || 'foo')
-			nrOfTrips++
-			out.emit('new-trip', m.tripId, m)
-			// todo: processs `m.nextStopovers`
+			out.emit('position', loc, m)
+			tripSeen(m.tripId, lineName)
 		}
 	}
 
 	const isStopoverObsolete = createIsStopoverObsolete(bbox)
-	// todo: remove trip if not found
-	const fetchTrip = (tripId, lineName) => async () => {
-		if (!running) return;
-		debugFetch('fetching trip', tripId)
+	const fetchTrip = async (id, lineName) => {
+		debugFetch('fetching trip', id, lineName)
 
-		let trip
-		try {
-			const t0 = Date.now()
-			trip = await hafas.trip(tripId, lineName) // todo: `opt.language`
-			onReqTime(Date.now() - t0)
-		} catch (err) {
-			out.emit('error', err)
-			return
-		}
+		const t0 = Date.now()
+		// todo: remove trip if not found
+		const trip = await hafas.trip(id, lineName) // todo: `opt.language`
+		onReqTime(Date.now() - t0)
+
 		if (trip.stopovers.every(isStopoverObsolete)) {
-			debugTrips('trip obsolete, removing', trip.stopovers.map(s => [s.stop.location, s.arrival || s.plannedArrival]))
-			trips.delete(tripId)
-			nrOfTrips--
-			out.emit('trip-obsolete', tripId, trip)
-			return
+			const st = trip.stopovers.map(s => [s.stop.location, s.arrival || s.plannedArrival])
+			debugTrips('trip obsolete', id, lineName, st)
+			tripObsolete(id)
+			return;
 		}
 
 		out.emit('trip', trip)
@@ -112,65 +126,80 @@ const createMonitor = (hafas, bbox, fetchTripsInterval = MINUTE, concurrency = 8
 				...stopover
 			}, trip)
 		}
+
+		tripSeen(trip.id, trip.line && trip.line.name || '')
 	}
 
 	let running = false
-	let fetchTilesTimer = null
-	let fetchTripsTimer = null
+	let fetchTilesTimer = null, tLastFetchTiles = -Infinity
+	let fetchTripsTimer = null, tLastFetchTrips = -Infinity
 
 	const fetchAllTiles = async () => {
 		if (!running) return;
+		debug('refreshing all tiles')
+		tLastFetchTiles = Date.now()
 
 		try {
-			await queue.addAll(tiles.map(fetchTile), {priority: 1})
+			await Promise.all(tiles.map(fetchTile))
 		} catch (err) {
 			out.emit('error', err)
 		}
 
+		debug('done refreshing tiles')
 		if (running) {
-			fetchTilesTimer = setTimeout(fetchAllTiles, fetchTilesInterval)
+			const tNext = Math.max(100, fetchTilesInterval - (Date.now() - tLastFetchTiles))
+			fetchTilesTimer = setTimeout(fetchAllTiles, tNext)
 		}
 	}
 
-	const fetchAllTrips = throttle(() => {
+	const fetchAllTrips = async () => {
 		if (!running) return;
+		debug('refreshing all trips')
+		tLastFetchTrips = Date.now()
 
-		for (const [tripId, lineName] of trips.entries()) {
-			queue.add(fetchTrip(tripId, lineName)) // todo: rejection?
+		try {
+			const jobs = []
+			for (const [id, lineName] of trips.entries()) {
+				jobs.push(fetchTrip(id, lineName))
+			}
+			await Promise.all(jobs)
+		} catch (err) {
+			out.emit('error', err)
 		}
 
+		debug('done refreshing trips')
 		if (running) {
-			fetchTripsTimer = setTimeout(fetchAllTrips, fetchTripsInterval)
+			const tNext = Math.max(100, fetchTripsInterval - (Date.now() - tLastFetchTrips))
+			fetchTripsTimer = setTimeout(fetchAllTrips, tNext)
 		}
-	}, fetchTripsInterval)
+	}
 
-	// todo: queue on error?
-	const out = new EventEmitter()
-
-	out.on('newListener', (eventName) => {
-		if (!WATCH_EVENTS.includes(eventName) || running) return;
+	const start = () => {
+		if (running) return false;
 		debug('starting monitor')
-
 		running = true
+
 		fetchAllTiles()
 		.then(fetchAllTrips)
 		.catch(() => {}) // silence rejection
-	})
-	// todo: should still be watching after on() on() off() [bug]
-	out.on('removeListener', (eventName) => {
-		if (!WATCH_EVENTS.includes(eventName) || !running) return;
-		debug('stopping monitor')
+	}
 
+	const stop = () => {
+		if (!running) return;
+		debug('stopping monitor')
 		running = false
-		queue.clear()
-		fetchAllTrips.cancel()
+
 		clearTimeout(fetchTilesTimer)
 		fetchTilesTimer = null
 		clearTimeout(fetchTripsTimer)
 		fetchTripsTimer = null
-	})
+	}
+
+	setImmediate(start)
 
 	out.hafas = hafas
+	out.start = start
+	out.stop = stop
 	return out
 }
 
