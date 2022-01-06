da3da239335d886169e9ca7213a2e721391e6923
Jannis R <mail@jannisr.de>
2022-01-06 17:54:09 +0100

    persist prioritised trip fetches
    see also #7

--- fetch-trips/priority-based.js
+++ fetch-trips/priority-based.js
@@ -2,6 +2,10 @@
 
 const {Gauge} = require('prom-client')
 const PromiseQueue = require('p-queue').default
+const redisReadRange = require('../lib/redis-read-range')
+const pkg = require('../package.json')
+
+const REDIS_TRIPS_QUEUE_NS = pkg.version.split('.')[0] + ':trips-prioritised-q:'
 
 const createPriorityBasedTripFetchingStrategy = (shouldFetchTrip, opt = {}) => {
 	const {
@@ -12,6 +16,9 @@ const createPriorityBasedTripFetchingStrategy = (shouldFetchTrip, opt = {}) => {
 	}
 
 	const priorityBasedTripFetchingStrategy = (monitor) => {
+		const {redis} = monitor
+		if (!redis) throw new Error('missing monitor.redis.')
+
 		// metrics
 		const queueSizeTotal = new Gauge({
 			name: 'fetch_trips_queue_size_total',
@@ -30,14 +37,37 @@ const createPriorityBasedTripFetchingStrategy = (shouldFetchTrip, opt = {}) => {
 			queueSizeTotal.set(queue.size)
 		}
 
+		const onError = (err) => monitor.emit('error', err)
+		const addTripFetchingTask = (taskId, priority, tripId, lineName, persist = true) => {
+			const fetchTrip = async () => {
+				await monitor.fetchTrip(tripId, lineName)
+				redis.del(taskId).catch(onError)
+			}
+			queue.add(fetchTrip, {priority})
+
+			if (persist) {
+				redis.set(taskId, JSON.stringify([priority, tripId, lineName])) // todo: expire?
+				.catch(onError)
+			}
+		}
+
 		monitor.on('position', (_, movement) => {
 			const priority = shouldFetchTrip(movement) // todo: async fn?
 			if (priority === null) return;
 
+			const taskId = REDIS_TRIPS_QUEUE_NS + Math.random().toString(16).slice(2, 6)
 			const tripId = movement.tripId
 			const lineName = movement.line && movement.line.name || '?'
-			queue.add(() => monitor.fetchTrip(tripId, lineName), {priority})
+			addTripFetchingTask(taskId, priority, tripId, lineName)
 		})
+
+		const readPersistedTasks = async () => {
+			for await (const [key, _] of redisReadRange(redis, REDIS_TRIPS_QUEUE_NS)) {
+				const taskId = key.slice(REDIS_TRIPS_QUEUE_NS.length)
+				const [priority, tripId, lineName] = JSON.parse(_)
+				addTripFetchingTask(taskId, priority, tripId, lineName, false)
+			}
+		}
 
 		const start = () => {
 			queue.start()
@@ -53,6 +83,8 @@ const createPriorityBasedTripFetchingStrategy = (shouldFetchTrip, opt = {}) => {
 		monitor.on('stop', stop)
 		// todo: check if the monitor is currently running
 		setImmediate(start)
+
+		readPersistedTasks().catch(onError)
 	}
 	return priorityBasedTripFetchingStrategy
 }
