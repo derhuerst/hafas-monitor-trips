2758f77fce63364de5c5e0689d09b273d4dd8a63
Jannis R <mail@jannisr.de>
2024-02-25 22:32:14 +0100

    find max nr of radar() results: tweak bbox algorithm ✅
    follow-up of b06c567
    follow-up of 98f042d
    related: #12

--- test.js
+++ test.js
@@ -4,7 +4,32 @@ const createHafas = require('vbb-hafas')
 const a = require('assert')
 const {Registry} = require('prom-client')
 const createMonitor = require('.')
+const {expandingBoundingBoxes} = require('./lib/find-max-radar-results')
 const fetchTrips = require('./fetch-trips')
+
+a.deepStrictEqual(
+	Array.from(expandingBoundingBoxes({
+		north: 54.52,
+		west: 6.54,
+		south: 51.29,
+		east: 6.65,
+	}))
+	.map(({north, west, south, east}, i) => [north, west, south, east]),
+	[
+		[53.0665,6.5895,52.7435,6.6005],
+		[53.1473,6.5868,52.6628,6.6032],
+		[53.2684,6.5826,52.5416,6.6074],
+		[53.4501,6.5764,52.3599,6.6136],
+		[53.7226,6.5672,52.0874,6.6228],
+		[54.1314,6.5532,51.6786,6.6368],
+		[54.7446,6.5324,51.0654,6.6576],
+		[55.6644,6.501, 50.1456,6.689 ],
+		[57.0441,6.454, 48.7659,6.736 ],
+		[59.1136,6.3836,46.6964,6.8064],
+		[62.2179,6.2778,43.5921,6.9122],
+		[66.8744,6.1193,38.9356,7.0707],
+	],
+)
 
 const METRICS = [
 	'hafas_reqs_total',
